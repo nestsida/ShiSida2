fe445a44a46d348bba2439f1ca098eb94eda398b
sarah sida <nestsida@gmail.com>
2021-11-09 15:07:46 -0500

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShiSida_Engineering.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
